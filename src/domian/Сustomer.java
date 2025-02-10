7954bc7f469d070c4d27a21c13d4928a5776b8d0
Aleksey-Shulzhenko <160366926+Aleksey-Shulzhenko@users.noreply.github.com>
2025-02-10 23:25:20 +0200

    main

--- src/domian/Сustomer.java
+++ src/domian/Сustomer.java
@@ -1,29 +1,14 @@
 package domain;
 
 public class Customer {
-    private int ID = 1;
-    private boolean isNew = true;
-    private double total = 1000.0;
+    private int ID;
+    private boolean isNew;
+    private double total;
 
+    // Конструктор с дефолтными значениями
     public Customer() {
-    }
-
-    // Геттери для доступу до атрибутів
-    public int getID() {
-        return ID;
-    }
-
-    public boolean isNew() {
-        return isNew;
-    }
-
-    public double getTotal() {
-        return total;
-    }
-
-    public void displayCustomerInfo() {
-        System.out.println("Customer ID: " + ID);
-        System.out.println("Is new customer: " + isNew);
-        System.out.println("Total purchases are: " + total);
+        this.ID = 1;   // Значение по умолчанию для ID
+        this.isNew = true; // Значение по умолчанию для isNew
+        this.total = 1000.0; // Значение по умолчанию для total
     }
 }
--- test/CustomerTest.java
+++ test/CustomerTest.java
@@ -3,9 +3,11 @@ package test;
 import domain.Customer;
 
 public class CustomerTest {
-    public static void main(String[] args)
-     {
+    public static void main(String[] args) {
+        // Створення об'єкта Customer
         Customer customer = new Customer();
+        
+        // Виведення властивостей об'єкта за допомогою методу displayCustomerInfo
         customer.displayCustomerInfo();
     }
 }
